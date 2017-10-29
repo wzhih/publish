b6215857b81561a956d8040af476707fec02ca44
winton <1309603684@qq.com>
2017-10-29 17:31:40 +0800

    会员管理的curd(没有检查)

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -19,4 +19,42 @@ class User extends Base
         return $this->fetch();
     }
 
+    public function update() {
+        $user = input('post.');
+
+        if (empty($user['password'])) {
+            unset($user['password']);
+        } else {
+            $user['password'] = md5($user['password']);
+        }
+
+        $result = Db::name('user')->data($user)->update();
+        if ($result !== false) {
+            $this->success('更新成功');
+        }
+
+        $this->error('更新失败');
+    }
+
+    public function add() {
+        $user = input('post.');
+
+        $result = Db::name('user')->data($user)->insert();
+        if ($result) {
+            $this->success('添加成功');
+        }
+
+        $this->error('添加失败');
+    }
+
+    public function delete() {
+        $uid = input('id');
+
+        $result = Db::name('user')->delete(['id' => $uid]);
+        if ($result) {
+            $this->success('删除成功');
+        }
+
+        $this->error('删除失败');
+    }
 }
