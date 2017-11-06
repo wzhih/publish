032a7b0eb664d4e5b6f91e54f5ec42ff12f3a994
winton <1309603684@qq.com>
2017-11-06 23:41:08 +0800

    增加后台控制前台轮播设置

---
+++ application/admin/controller/Front.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: winton
+ * Date: 2017/11/6
+ * Time: 21:26
+ */
+
+namespace app\admin\controller;
+
+use think\Db;
+
+class Front extends Base
+{
+    public function getSwiper()
+    {
+        $data = Db::name('swiper')->select();
+
+        $this->assign('data', $data);
+        return $this->fetch();
+    }
+
+    public function setSwiper()
+    {
+        $data = input('post.');
+        $photo_url = uploading('photo_url', 'static'. DS .'portal'. DS .'img'. DS .'swiper');
+
+        if ($photo_url) {
+            $data['photo_url'] = $photo_url;
+        }
+
+        $result = Db::name('swiper')->data($data)->update();
+        if ($result) {
+            $this->success('更新成功');
+        }
+
+        $this->error('更新失败');
+    }
+}
