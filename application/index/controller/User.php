e0d7e1fc20218f144d4b3e4285bb8fd47ec23a36
winton <1309603684@qq.com>
2018-01-06 00:44:51 +0800

    增加个人中心页面

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -14,6 +14,19 @@ class User extends Base
 {
     public function user()
     {
+        $user = session('user');
+        $order = Db::name('order')->where([
+            'u_id' => $user['id']
+        ])
+            ->select();
 
+        $offset = mt_rand(99, 1999);
+        $rand = Db::name('publication')->limit($offset, 6)->select();
+
+        $this->assign('cid', '');
+        $this->assign('rand', $rand);
+        $this->assign('order', $order);
+        $this->assign('user', $user);
+        return $this->fetch();
     }
 }
