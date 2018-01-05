685f2388e557ba6f6a6e49d3c6891fb14687062c
winton <1309603684@qq.com>
2018-01-05 23:53:24 +0800

    新增订单页面(未写支付功能)

--- application/index/controller/Shop.php
+++ application/index/controller/Shop.php
@@ -8,7 +8,7 @@
 
 namespace app\index\controller;
 
-
+use Throwable;
 use think\Db;
 
 class Shop extends Base
@@ -147,7 +147,8 @@ class Shop extends Base
         return json_result($result);
     }
 
-    //结算购物车，$checkId是购物车表id
+    //结算购物车，$checkId是购物车表id，
+    //生成订单，并跳转到订单页面
     public function settleCart()
     {
         $checkId = input('checkId/a');
@@ -155,8 +156,125 @@ class Shop extends Base
             return json_result(false, '参数错误');
         }
 
-        return json_result($checkId);
+        //开始事务
+        Db::startTrans();
+        $user = session('user');
+
+        try {
+            $data = Db::name('cart')
+                ->alias([
+                    'cart' => 'c',
+                    'publication' => 'p',
+                ])
+                ->join('publication', 'c.p_id = p.id')
+                ->field("c.*, p.price")
+                ->where([
+                    'c.u_id' => $user['id']
+                ])
+                ->whereIn('c.id', $checkId)
+                ->select();
+
+            $total_price = 0;
+            foreach ($data as $value)
+            {
+                $total_price = bcadd($total_price, bcmul($value['quantity'], $value['price'], 2), 2);
+            }
+            //先插入订单表
+            $order_id = Db::name('order')->insertGetId([
+                'u_id' => $user['id'],
+                'total_price' => $total_price,
+                'order_time' => date('Y-m-d'),
+            ]);
+
+            $op = [];
+            foreach ($data as $value)
+            {
+                $op[] = [
+                    'o_id' => $order_id,
+                    'p_id' => $value['p_id'],
+                    'quantity' => $value['quantity'],
+                    'price' => $value['price'], //单件商品单价
+                ];
+            }
+            $ops = Db::name('order_publication')->insertAll($op);
+
+            $del = Db::name('cart')->whereIn('id', $checkId)->delete();
+
+            Db::commit();
+            return json_result(true, $order_id);
+        } catch (Throwable $e) {
+            Db::rollback();
+            return json_result(false, $e.getMessage());
+        }
+
     }
 
+    public function order()
+    {
+        $order_id = input('order_id');
+        if (!$order_id) {
+            $this->error('没有此订单');
+        }
+        $user = session('user');
 
+        $contact = Db::name('contact')->where("u_id = {$user['id']}")->select();
+
+        $data = Db::name('user')->alias([
+            'user'  => 'u',
+            'order' => 'o',
+            'order_publication' => 'op',
+            'publication'   => 'p',
+        ])
+            ->join('order', 'o.u_id=u.id')
+            ->join('order_publication', 'op.o_id')
+            ->join('publication', 'op.p_id = p.id')
+            ->where([
+                'u.id' => $user['id'],
+                'o.id' => $order_id,
+                'op.o_id' => $order_id,
+            ])
+            ->field('o.id, p.id as p_id, p.cover, p.`name`, p.author, op.quantity, op.price, o.total_price')
+            ->select();
+
+        $total_price = 0;
+        if (isset($data[0]['total_price'])) {
+            $total_price = $data[0]['total_price'];
+        }
+
+        $this->assign('cid', '');
+        $this->assign('o_id', $order_id);
+        $this->assign('contact', $contact);
+        $this->assign('data', $data);
+        $this->assign('total_price', $total_price);
+        return $this->fetch();
+    }
+
+    //订单页面确认支付（其实是更新订单的地址）
+    public function payment()
+    {
+        $contact_id = input('contact_id');
+        $order_id = input('order_id');
+
+        if (!$contact_id || !$order_id) {
+            return json_result(false, '参数错误');
+        }
+
+        $result = Db::name('order')->where("id = $order_id")->update(['c_id' => $contact_id]);
+
+        return json_result(true);
+    }
+
+    public function paymentConfirm()
+    {
+        //支付成功，订单记录状态修改
+        $order_id = input('order_id');
+
+        if (!$order_id) {
+            return json_result(false, '参数错误');
+        }
+
+        $result = Db::name('order')->where("id = $order_id")->update(['status' => 1]);
+
+        return $this->success('支付成功，跳转到订单列表。');
+    }
 }
---
+++ application/index/controller/User.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: winton
+ * Date: 2018/1/5
+ * Time: 15:31
+ */
+
+namespace app\index\controller;
+
+use think\Db;
+
+class User extends Base
+{
+    public function user()
+    {
+
+    }
+}
