7d83d62b47d4614982ec2373aa9ca9f6bde00baf
winton <1309603684@qq.com>
2018-03-04 20:32:19 +0800

    销售统计，增加每月销售数量曲线图

--- application/admin/controller/Sale.php
+++ application/admin/controller/Sale.php
@@ -53,5 +53,32 @@ class Sale extends Base
         return $this->fetch();
     }
 
+    public function chart()
+    {
+        $year = input('year');
+        $mon = input('mon');
+        if (is_numeric($mon) && $mon > 0 && $mon < 13) {
+            if (!is_numeric($year)) {
+                //$year年$mon月每日销售情况
+                $year = date('Y');
+            }
+            $time = $year . '-' . sprintf('%02d', $mon);
+        } else {
+            //本月每日销售情况
+            $time = date('Y-m');
+        }
+
+        $data = Db::name('order_publication op')
+            ->join('`order` o', "op.o_id = o.id", 'LEFT')
+            ->where('order_time', 'like', "$time%")
+            ->field("DATE_FORMAT(order_time,'%e') as `day`, SUM(op.quantity) as sum")
+            ->group('`day`')
+            ->select();
+
+        $this->assign('t', date('t', strtotime($mon)));
+        $this->assign('calcula', json_encode($data));
+        $this->assign('datetime', $time);
+        return $this->fetch();
+    }
 
 }
